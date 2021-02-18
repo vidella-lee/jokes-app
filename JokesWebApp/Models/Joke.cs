6bdabc06e0794b3284c2e134b5abdbd4a4875213
Liz <lee.anewed@gmail.com>
2021-02-18 08:27:31 -0600

    Initialized basic ASP.NET Core Web App

---
+++ JokesWebApp/Models/Joke.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        //getters/setters
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+
+        public string JokeAnswer { get; set; }
+
+        //constructor
+        public Joke()
+        {
+
+        }
+    }
+}
